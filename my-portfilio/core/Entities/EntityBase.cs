05e4645c0e1da675dbd02ec01e34b62019e1a91a
MohamedEl-mowafy <eng.mohamedelmwafy@gmail.com>
2023-02-01 19:50:46 +0300

    myportflio

---
+++ my-portfilio/core/Entities/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace core.Entities
+{
+	public class EntityBase
+	{
+		public EntityBase()
+		{
+		}
+	}
+}
+
---
+++ my-portfilio/core/Entities/Owner.cs
@@ -0,0 +1,10 @@
+﻿namespace core.Entities
+{
+    public class Owner : EntityBase
+    {
+        public string FullName { get; set; }
+        public string MyProperty { get; set; }
+        public string Avtar { get; set; }
+    }
+}
+
